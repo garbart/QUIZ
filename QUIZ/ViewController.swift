c6666bc8c3a4ea44ad1f76cd44208128c4507153
Artem Garbart <garbart@yandex.ru>
2023-01-03 12:36:03 +0300

    Initial Commit

---
+++ QUIZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QUIZ
+//
+//  Created by Артем Гарбарт on 03.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
